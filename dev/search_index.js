9aa75bfdecea5173fd8ee879463fe4d3fa2b840a
Documenter.jl <documenter@juliadocs.github.io>
2023-08-14 21:29:08 +0000

    build based on a286332

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JPerplex","category":"page"},{"location":"#JPerplex","page":"Home","title":"JPerplex","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for JPerplex.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JPerplex]","category":"page"}]
+}
